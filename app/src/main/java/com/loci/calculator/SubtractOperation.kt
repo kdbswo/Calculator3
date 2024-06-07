fe48193c06e21cdc50d99384b4171a693d3473dc
kdbswo <kdbswo@gmail.com>
2024-06-07 20:27:25 +0900

    Update SubtractOperation

--- app/src/main/java/com/loci/calculator/SubtractOperation.kt
+++ app/src/main/java/com/loci/calculator/SubtractOperation.kt
@@ -2,6 +2,6 @@ package com.loci.calculator
 
 class SubtractOperation: Calculator() {
     override fun calculator(input: Int, result: Int): Int {
-        return input - result
+        return   result - input
     }
 }
